48666af66ed269ae803c7d4bbc0f71269bbf4d1d
cl <chencassc@gmail.com>
2025-03-10 16:09:03 +0800

    update go emvm test

--- go-evm/goevm_real_txs/main.go
+++ go-evm/goevm_real_txs/main.go
@@ -3,151 +3,51 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"math/big"
 	"os"
+	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/tracing"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/triedb"
-	"github.com/holiman/uint256"
+	"github.com/ethereum/go-ethereum/tests"
 )
 
-type stEnv struct {
-	Coinbase      common.Address        `json:"currentCoinbase"      gencodec:"required"`
-	Difficulty    *math.HexOrDecimal256 `json:"currentDifficulty"    gencodec:"optional"`
-	Random        *math.HexOrDecimal256 `json:"currentRandom"        gencodec:"optional"`
-	GasLimit      uint64                `json:"currentGasLimit"      gencodec:"required"`
-	Number        uint64                `json:"currentNumber"        gencodec:"required"`
-	Timestamp     uint64                `json:"currentTimestamp"     gencodec:"required"`
-	BaseFee       *math.HexOrDecimal256 `json:"currentBaseFee"       gencodec:"optional"`
-	ExcessBlobGas *uint64               `json:"currentExcessBlobGas" gencodec:"optional"`
-}
-
-type stAuthorization struct {
-	ChainID *math.HexOrDecimal256 `json:"chainId" gencodec:"required"`
-	Address common.Address        `json:"address" gencodec:"required"`
-	Nonce   uint64                `json:"nonce" gencodec:"required"`
-	V       uint8                 `json:"v" gencodec:"required"`
-	R       *math.HexOrDecimal256 `json:"r" gencodec:"required"`
-	S       *math.HexOrDecimal256 `json:"s" gencodec:"required"`
-}
-
-type stTransaction struct {
-	GasPrice             *math.HexOrDecimal256 `json:"gasPrice"`
-	MaxFeePerGas         *math.HexOrDecimal256 `json:"maxFeePerGas"`
-	MaxPriorityFeePerGas *math.HexOrDecimal256 `json:"maxPriorityFeePerGas"`
-	Nonce                uint64                `json:"nonce"`
-	To                   string                `json:"to"`
-	Data                 []string              `json:"data"`
-	AccessLists          []*types.AccessList   `json:"accessLists,omitempty"`
-	GasLimit             []uint64              `json:"gasLimit"`
-	Value                []string              `json:"value"`
-	PrivateKey           []byte                `json:"secretKey"`
-	Sender               *common.Address       `json:"sender"`
-	BlobVersionedHashes  []common.Hash         `json:"blobVersionedHashes,omitempty"`
-	BlobGasFeeCap        *math.HexOrDecimal256 `json:"maxFeePerBlobGas,omitempty"`
-	AuthorizationList    []*stAuthorization    `json:"authorizationList,omitempty"`
-}
-
-type StateTest struct {
-	Env stEnv              `json:"env"`
-	Pre types.GenesisAlloc `json:"pre"`
-	Tx  stTransaction      `json:"transaction"`
-	Out hexutil.Bytes      `json:"out"`
-}
-
-func benchmark_file(fname string) {
-	src, err := os.ReadFile(fname)
+func runBenchmark(iterations int, testFile string) {
+	src, err := os.ReadFile(testFile)
 	if err != nil {
-		panic("read file content failed")
+		panic(fmt.Errorf("failed to read test file: %v", err))
 	}
 
-	var testsByName map[string]StateTest
-
+	var testsByName map[string]tests.StateTest
 	if err := json.Unmarshal(src, &testsByName); err != nil {
-		panic(fmt.Errorf("unable to read test file %s: %w", fname, err))
+		panic(fmt.Errorf("failed to unmarshal test data: %v", err))
 	}
-
-	fmt.Printf("Parsed content: %v\n", testsByName)
 
 	for _, test := range testsByName {
+		st := tests.StateSubtest{Fork: "Shanghai"}
+		cfg := vm.Config{}
 
-		// Get tx
-		to := common.HexToAddress(test.Tx.To)
-		from := test.Tx.Sender
-		// value from json.Tx.value[0]
-		value := new(big.Int)
-		value, ok := value.SetString(test.Tx.Value[0], 16)
-		if !ok {
-			panic("Invalid value")
-		}
-
-		gasLimit := test.Tx.GasLimit[0]
-		gasPrice := test.Tx.GasPrice
-		msgData := common.FromHex(test.Tx.Data[0])
-
-		msg := &core.Message{
-			To:       &to,
-			From:     *from,
-			Nonce:    test.Tx.Nonce,
-			Value:    value,
-			GasLimit: gasLimit,
-			GasPrice: gasPrice,
-			Data:     msgData,
-		}
-
-		tconf := &triedb.Config{Preimages: true}
-		triedb := triedb.NewDatabase(rawdb.NewMemoryDatabase(), tconf)
-		sdb := state.NewDatabase(triedb, nil)
-		stateDB, err := state.New(common.Hash{}, sdb)
+		// Warm-up iteration
+		_, _, _, err = test.RunNoVerify(st, cfg, false, rawdb.HashScheme)
 		if err != nil {
-			panic(fmt.Errorf("Error creating state database: %v\n", err))
+			panic(fmt.Errorf("warm-up failed: %v", err))
 		}
 
-		// load states from json
-		for address, account := range test.Pre {
-			stateDB.SetBalance(address, uint256.MustFromBig(account.Balance), tracing.BalanceChangeTouchAccount)
-			stateDB.SetNonce(address, account.Nonce, tracing.NonceChangeAuthorization)
-			stateDB.SetCode(address, account.Code)
-			// load storage key-values
-			for key, value := range account.Storage {
-				stateDB.SetState(address, key, value)
+		start := time.Now()
+		for range iterations {
+			_, _, _, err = test.RunNoVerify(st, cfg, false, rawdb.HashScheme)
+			if err != nil {
+				panic(fmt.Errorf("test execution failed: %v", err))
 			}
 		}
+		duration := time.Since(start)
 
-		// block context
-
-		blockContext := vm.BlockContext{
-			Transfer:    core.Transfer,
-			GetHash:     func(n uint64) common.Hash { return common.Hash{} },
-			Coinbase:    common.Address{},
-			BlockNumber: big.NewInt(9069001), // Set to >= IstanbulBlock (9069000)
-			Time:        test.Env.Timestamp,
-			Difficulty:  test.Env.Difficulty,
-			GasLimit:    test.Env.GasLimit,
-			BaseFee:     test.Env.BaseFee,
-		}
-
-		chainConfig := params.MainnetChainConfig
-		vmConfig := vm.Config{}
-		evm := vm.NewEVM(blockContext, stateDB, chainConfig, vmConfig)
-
-		_, _, err = evm.Call(msg.From, *msg.To, msg.Data, msg.GasLimit, uint256.MustFromBig(msg.Value))
-		if err != nil {
-			panic(fmt.Errorf("Error calling EVM: %v\n", err))
-		}
+		fmt.Printf("Benchmark %s\t%.2f ms / transaction\n", testFile, float64(duration.Nanoseconds())/float64(iterations)/1000000.0)
+		break // Only run first test
 	}
-
 }
 
 func main() {
-	benchmark_file("../uniswap.bench.input.json")
+	const iterations = 10000
+	runBenchmark(iterations, "../erc20.bench.input.json")
+	runBenchmark(iterations, "../uniswap.bench.input.json")
 }
